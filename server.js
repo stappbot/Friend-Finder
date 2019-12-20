550791b7fb8bb944687416edb29145b3815a3462
Jessica Stapp <stapp9@gmail.com>
2019-12-20 12:58:00 -0800

    routing and html file updates

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+// var exphbs = require("express-handlebars");
+// var mysql = require("mysql");
+​var bodyParser = require("body-parser");
+var app = express();
+​
+var PORT = process.env.PORT || 8040;
+
+//JSON parser
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+
+app.use(bodyParser.json({type: "application/*+json"}))
+app.use(bodyParser.raw({type:"application/vdn.custom-type"}))
+app.use(bodyParser.text({type:"text/html"}))
+
+require("./app/routing/html-routes.js")(app);
+
+​
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// ​
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+//listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
